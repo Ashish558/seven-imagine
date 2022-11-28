7691b75fb182808093d59a1e581c747c46e2d639
shakib-mia <smdshakibmia2001@gmail.com>
2022-11-28 21:45:09 +0600

    indicator fixed

--- src/components/NumericSteppers/NumericSteppers.js
+++ src/components/NumericSteppers/NumericSteppers.js
@@ -7,10 +7,10 @@ export default function NumericSteppers({ totalSteps, currentStep }) {
    return (
       <div className='mt-6 mb-120 flex'>
          {[...Array(totalSteps)].map((x, i) =>
-            <button key={i} className={`w-9 h-9 border border-primary rounded-full mr-7 font-bold
-             ${i + 1 < currentStep ? 'bg-primary text-white' : ''} 
+            <button key={i} className={`w-[36px] h-[36px] border border-primary rounded-full mr-7 font-bold
+             ${i + 1 < currentStep ? 'bg-primary text-[#F3F5F7]' : ''} 
              ${i + 1 === currentStep ? 'before:hidden bg-white border-3 border-primary text-primary' : ''} 
-             ${i + 1 > currentStep ? 'opacity-50 before:hidden text-primary bg-white' : ''} 
+             ${i + 1 > currentStep ? 'opacity-50 before:hidden text-[#F3F5F7] bg-[#7152EB]' : ''} 
              transition 
              ${styles.line} `}
             // onClick={() => handleClick(i + 1)}
