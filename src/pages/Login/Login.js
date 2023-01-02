d0394f6773054d28845ffacd32bc898af48e0e79
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-02 13:40:54 +0600

    login message fixed

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -15,12 +15,13 @@ import CarouselImg from "../../assets/form/image-1.png";
 import styles from './Login.module.css'
 
 export default function Login({ setLoginFormActive }) {
-
+   const emailValidation = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
    const [isPasswordForgot, setIsPasswordForgot] = useState(false);
    const [resetPasswordActive, setResetPasswordActive] = useState(false);
    const [loginActive, setLoginActive] = useState(true);
    const [email, setEmail] = useState("");
    const [password, setPassword] = useState("");
+   const [error, setError] = useState({})
 
    const dispatch = useDispatch();
 
@@ -35,12 +36,19 @@ export default function Login({ setLoginFormActive }) {
 
    const handleSubmit = () => {
       loginUser({ email, password }).then((res) => {
-         console.log(res);
-         localStorage.setItem("token", res.data.data.token);
-         localStorage.setItem("role", res.data.data.role);
-         localStorage.setItem("userId", res.data.data.userId);
-         dispatch(updateIsLoggedIn(true));
-      });
+         console.log(res.error);
+         // console.log(!res.error);
+         if(res.data) {
+            localStorage.setItem("token", res.data.data.token);
+            localStorage.setItem("role", res.data.data.role);
+            localStorage.setItem("userId", res.data.data.userId);
+            dispatch(updateIsLoggedIn(true));
+         }
+
+         if(res.error) {
+            setError(res.error);
+         }
+      })
    };
 
    const props = { setActiveFrame, setResetPasswordActive };
@@ -66,13 +74,14 @@ export default function Login({ setLoginFormActive }) {
                         Icon={EmailIcon}
                         iconSize='medium'
                         placeholder="Email address"
-                        parentClassName="mb-6"
+                        parentClassName="mb-6 relative"
                         label="Email Address"
                         labelClassname="ml-2 mb-2"
                         inputClassName="bg-transparent"
                         inputContainerClassName='border pt-3 pb-3'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        error={error.status === 404 ? error.data.message : undefined}
                      />
 
                      <InputField
@@ -87,6 +96,7 @@ export default function Login({ setLoginFormActive }) {
                         inputContainerClassName='border pt-3 pb-3'
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        error={error.status === 401 ? error.data.message : undefined}
                      />
                      <p
                         className="text-secondary text-xs inline-block cursor-pointer font-semibold ml-2"
@@ -98,7 +108,7 @@ export default function Login({ setLoginFormActive }) {
                      </p>
 
                      <button
-                        disabled={false}
+                        disabled={!(emailValidation.test(email) && password.length > 0)}
                         className="w-full bg-primaryDark disabled:bg-pink pt-3.5 pb-3.5 mt-12 rounded-10 text-white text-lg"
                         onClick={handleSubmit}
                      >
