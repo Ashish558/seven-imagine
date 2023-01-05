08c7c2a07571784de11cc028c980928ab3ec0a0c
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-05 21:57:25 +0600

    ledger page popup made

--- src/components/ParentDashboardHeader/ParentDashboardHeader.js
+++ src/components/ParentDashboardHeader/ParentDashboardHeader.js
@@ -14,12 +14,15 @@ import ImageSlideshow from "../ImageSlideshow/ImageSlideshow";
 import { useLazyGetUserDetailQuery } from "../../app/services/users";
 import { useSelector } from "react-redux";
 import InputSelect from "../InputSelect/InputSelect";
+import Ledger from "./../../pages/Ledger/Ledger"
 import { useLazyPayBalanceQuery } from "../../app/services/dashboard";
 
 const ParentDashboardHeader = () => {
    const [images, setImages] = useState([])
    const [user, setUser] = useState({})
-   const [associatedStudents, setAssociatedStudents] = useState([])
+   const [associatedStudents, setAssociatedStudents] = useState([]);
+
+   const [ledgerVisible, setLedgerVisible] = useState(false)
 
    const [getUserDetail, userDetailResp] = useLazyGetUserDetailQuery()
    const [fetchSettings, fetchSettingsResp] = useLazyGetSettingsQuery()
@@ -93,98 +96,102 @@ const ParentDashboardHeader = () => {
    }
 
    return (
-      <div
-         className="flex 2xl:gap-[78px] xl:gap-[50px] ml-[55px]"
-         id={styles.parentDashboardHeader}
-      >
-         <div className="w-2/3">
-            <div className="flex" style={{ gap: 16 }}>
-               <div className="w-2/3 flex items-center" id={styles.explore}>
-                  <div className="flex mx-auto">
-                     <div className="w-1/2" id={styles.exploreLeft}>
-                        <h2 className="">
-                           This fall get help from our Admission
-                           Experts.
-                        </h2>
-                        <button className="ml-[32px] text-sm 2xl:ml-[46px] bg-[#f3f5f7] rounded-[5px] py-[8px] px-[15px]">
-                           Know More {">"}
-                        </button>
-                     </div>
-                     <div className="w-1/2 flex items-center px-2 pr-4" id={styles.exploreBgDisable}>
-                        {/* <div className="relative w-full h-fll">
-                           {
-                              images.length > 0 ? <img src={images[0]} alt="" />
-                                 :
-                                 <img src={explore} alt="" />
-                           }
-                        </div> */}{
-                           images.length >= 1 &&
-                           <ImageSlideshow images={images} />
-                        }
-                     </div>
-                  </div>
-               </div>
-
-               <div className="w-1/3" id={styles.availableCredit}>
-                  <div className="flex justify-between">
-                     <h3 className="2xl:text-[19.6px] font-semibold">Available Credit</h3>
-                     <img src={i} alt="" />
-                  </div>
-
-                  <div id={styles.creditBalance}>
-                     <p className="whitespace-nowrap text-3xl leading-none mb-1" >
-                     {credits} USD
-                     </p>
-                     <p className="text-[13.17px] font-bold cursor-pointer"
-                        onClick={() => navigate('/ledger')}>
-                        View details
-                     </p>
-                  </div>
-                  <button className={styles.btnDark} onClick={handlePay} >
-                     Pay Now: $ {amountToPay}
-                  </button>
-               </div>
-            </div>
-         </div>
-
+      <>
+         {ledgerVisible && <Ledger setLedgerVisible={setLedgerVisible} />}
          <div
-            className="w-1/3"
+            className="flex 2xl:gap-[78px] xl:gap-[50px] ml-[55px]"
+            id={styles.parentDashboardHeader}
          >
-            <div className="flex justify-between items-center px-[11px]">
-               <h2 className="text-[#4715D7] font-semibold text-[21px] mt-[16px]">Your Student</h2>
-               {/* <img src={rightArrow} className="h-[15px] w-[15px]" alt="" /> */}
-               {associatedStudents.length > 0 &&
-                  <InputSelect optionType='object'
-                     parentClassName='mb-2'
-                     inputContainerClassName='pt-1 pb-1'
-                     optionData={associatedStudents.map(item => ({ _id: item._id, value: item.name }))}
-                     optionClassName='w-[130px] text-sm'
-                     value={selectedStudent === null ? '' : selectedStudent.value}
-                     onChange={val => setSelectedStudent(val)} />}
-            </div>
-            <div className={`item ${styles.student} w-100 px-[22px] 2xl:px-[32px] 2xl:py-[13px]`}>
-               <div className="flex items-center">
-                  <div className="w-1/2">
-                     <h2>
-                        {/* {selectedStudent !== null && 'Joseph Brown'}  */}
-                        {selectedStudent === null ? 'Dummy name' :
-                           selectedStudent.value}
-                     </h2>
-                     <h6 className="text-[10px]">SAT Tutoring <br />Subject Tutoring</h6>
-                     <Link className="btn-gold"
-                        to={selectedStudent !== null && `/profile/student/${selectedStudent._id}`}>
-                        View Profile
-                     </Link>
+            <div className="w-2/3">
+               <div className="flex" style={{ gap: 16 }}>
+                  <div className="w-2/3 flex items-center" id={styles.explore}>
+                     <div className="flex mx-auto">
+                        <div className="w-1/2" id={styles.exploreLeft}>
+                           <h2 className="">
+                              This fall get help from our Admission
+                              Experts.
+                           </h2>
+                           <button className="ml-[32px] text-sm 2xl:ml-[46px] bg-[#f3f5f7] rounded-[5px] py-[8px] px-[15px]">
+                              Know More {">"}
+                           </button>
+                        </div>
+                        <div className="w-1/2 flex items-center px-2 pr-4" id={styles.exploreBgDisable}>
+                           {/* <div className="relative w-full h-fll">
+                              {
+                                 images.length > 0 ? <img src={images[0]} alt="" />
+                                    :
+                                    <img src={explore} alt="" />
+                              }
+                           </div> */}{
+                              images.length >= 1 &&
+                              <ImageSlideshow images={images} />
+                           }
+                        </div>
+                     </div>
                   </div>
-                  <div className="w-1/2 flex justify-end">
-                     <img src={shivam} alt="" />
+
+                  <div className="w-1/3" id={styles.availableCredit}>
+                     <div className="flex justify-between">
+                        <h3 className="2xl:text-[19.6px] font-semibold">Available Credit</h3>
+                        <img src={i} alt="" />
+                     </div>
+
+                     <div id={styles.creditBalance}>
+                        <p className="whitespace-nowrap text-3xl leading-none mb-1" >
+                        {credits} USD
+                        </p>
+                        <p className="text-[13.17px] font-bold cursor-pointer"
+                           onClick={() => setLedgerVisible(true)}>
+                           View details
+                        </p>
+                     </div>
+                     <button className={styles.btnDark} onClick={handlePay} >
+                        Pay Now: $ {amountToPay}
+                     </button>
                   </div>
                </div>
             </div>
 
-         </div>
+            <div
+               className="w-1/3"
+            >
+               <div className="flex justify-between items-center px-[11px]">
+                  <h2 className="text-[#4715D7] font-semibold text-[21px] mt-[16px]">Your Student</h2>
+                  {/* <img src={rightArrow} className="h-[15px] w-[15px]" alt="" /> */}
+                  {associatedStudents.length > 0 &&
+                     <InputSelect optionType='object'
+                        parentClassName='mb-2'
+                        inputContainerClassName='pt-1 pb-1'
+                        optionData={associatedStudents.map(item => ({ _id: item._id, value: item.name }))}
+                        optionClassName='w-[130px] text-sm'
+                        value={selectedStudent === null ? '' : selectedStudent.value}
+                        onChange={val => setSelectedStudent(val)} />}
+               </div>
+               <div className={`item ${styles.student} w-100 px-[22px] 2xl:px-[32px] 2xl:py-[13px]`}>
+                  <div className="flex items-center">
+                     <div className="w-1/2">
+                        <h2>
+                           {/* {selectedStudent !== null && 'Joseph Brown'}  */}
+                           {selectedStudent === null ? 'Dummy name' :
+                              selectedStudent.value}
+                        </h2>
+                        <h6 className="text-[10px]">SAT Tutoring <br />Subject Tutoring</h6>
+                        <Link className="btn-gold"
+                           to={selectedStudent !== null && `/profile/student/${selectedStudent._id}`}>
+                           View Profile
+                        </Link>
+                     </div>
+                     <div className="w-1/2 flex justify-end">
+                        <img src={shivam} alt="" />
+                     </div>
+                  </div>
+               </div>
 
-      </div>
+            </div>
+
+         </div>
+      
+      </>
    );
 };
 
