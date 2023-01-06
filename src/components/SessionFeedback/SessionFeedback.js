dc2b3ce5149046a82ab22ec2ad2115eef9c93a14
Ashish558 <ashishprasad2001ap@gmail.com>
2023-01-06 15:56:24 +0530

    profile img added

--- src/components/SessionFeedback/SessionFeedback.js
+++ src/components/SessionFeedback/SessionFeedback.js
@@ -9,12 +9,12 @@ const SessionFeedback = () => {
     return (
         <div id={styles.sessionFeedbackContainer} className="h-[382px] bg-white w-10/12 py-[21px] mt-[33px] rounded-[20px]">
             <div id={styles.sessionFeedback} className="bg-white px-[28px] rounded-[20px] h-full overflow-y-auto">
-                <TestItem name="name" status="due date" date="june 20, 2022" action="Start" marks="1250/1250" />
+                {/* <TestItem name="name" status="due date" date="june 20, 2022" action="Start" marks="1250/1250" />
                 <TestItem name="SAT Series #01" status="due date" date="june 20, 2022" action="Continue" />
                 <TestItem name="Ex. Starte" status="due date" date="june 20, 2022" action="Start" marks="1250/1250" />
                 <TestItem name="SAT B2" status="Completed" date="june 20, 2022" /> 
-              
-               
+               */}
+
             </div>
         </div>
     );
